aa29fd3e0f107ba0f47f8c74ee8fbe5216ef03de
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-03-31 14:54:25 +0100

    adding zombienet test

--- tests/zombie/test_basic.ts
+++ tests/zombie/test_basic.ts
@@ -1,27 +1,26 @@
 import { expect, describeSuite, beforeAll, ApiPromise, ethers } from "@moonsong-labs/moonwall-cli";
-import { BALTATHAR_ADDRESS, alith} from "@moonsong-labs/moonwall-util"
-import "@moonbeam-network/api-augment"
+import { BALTATHAR_ADDRESS, alith } from "@moonsong-labs/moonwall-util";
+import "@moonbeam-network/api-augment";
 
 describeSuite({
   id: "Z1",
   title: "Zombie Test Suite",
   foundationMethods: "zombie",
-  testCases: function ({ it,context, log }) {
+  testCases: function ({ it, context, log }) {
+    let paraApi: ApiPromise;
+    let relayApi: ApiPromise;
 
-    let paraApi: ApiPromise
-    let relayApi: ApiPromise
-
-    beforeAll(()=>{
-      paraApi = context.getSubstrateApi({type: "moon"})
-      relayApi = context.getSubstrateApi({type: "polkadotJs"})
-    })
+    beforeAll(() => {
+      paraApi = context.getSubstrateApi({ type: "moon" });
+      relayApi = context.getSubstrateApi({ type: "polkadotJs" });
+    });
 
     it({
       id: "T01",
       title: "This is a version test case",
       test: function () {
-        const rt = relayApi.consts.system.version.specVersion.toNumber()
-        expect(rt).to.be.greaterThan(0)
+        const rt = relayApi.consts.system.version.specVersion.toNumber();
+        expect(rt).to.be.greaterThan(0);
       },
     });
 
@@ -29,7 +28,7 @@ describeSuite({
       id: "T02",
       title: "This is a network name test case",
       test: function () {
-        const network = paraApi.consts.system.version.specName.toString()
+        const network = paraApi.consts.system.version.specName.toString();
         expect(network).to.contain("moonbase");
       },
     });
@@ -37,24 +36,27 @@ describeSuite({
     it({
       id: "T03",
       title: "This is a network txn test case",
+      timeout: 60000,
       test: async function () {
-        const balBefore = (await paraApi.query.system.account(BALTATHAR_ADDRESS)).data.free
-        const unsub = await paraApi.tx.balances.transfer(BALTATHAR_ADDRESS, ethers.parseEther("2")).signAndSend(alith, (result)=>{
+        const balBefore = (await paraApi.query.system.account(BALTATHAR_ADDRESS)).data.free;
 
-if (result.status.isInBlock){
-  console.log("in block")
-}
-          if (result.status.isFinalized){
-            unsub()
-          }
-        })
+        await new Promise((resolve) => {
+          paraApi.tx.balances
+            .transfer(BALTATHAR_ADDRESS, ethers.parseEther("2"))
+            .signAndSend(alith, ({ status, events }) => {
+              if (status.isInBlock) {
+                log("Transaction is in block");
+              }
+              if (status.isFinalized) {
+                log("Transaction is finalized!");
+                resolve(events);
+              }
+            });
+        });
 
-        const balAfter = (await paraApi.query.system.account(BALTATHAR_ADDRESS)).data.free
-        log(balBefore.toHuman())
-        log(balAfter.toHuman())
-        expect(balBefore.lt(balAfter)).to.be.true
+        const balAfter = (await paraApi.query.system.account(BALTATHAR_ADDRESS)).data.free;
+        expect(balBefore.lt(balAfter)).to.be.true;
       },
     });
-
   },
 });
