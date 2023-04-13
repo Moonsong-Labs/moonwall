e4c8a28d67634fbd9b4d05eda1f1197e848f8ea0
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-04-13 14:40:58 +0100

    skip chopsticks test

--- tests/chopsticks/test-chopsticks-basic.ts
+++ tests/chopsticks/test-chopsticks-basic.ts
@@ -97,7 +97,7 @@ describeSuite({
       id: "T5",
       title: "Do an upgrade test",
       timeout: 120000,
-      // modifier: "only",
+      modifier: "skip",
       test: async function () {
         const rtBefore = api.consts.system.version.specVersion.toNumber();
         await context.upgradeRuntime(context);
