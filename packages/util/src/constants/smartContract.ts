e68674ae3a6fa553a04fa58c69aa5fc2aabd3f7e
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-04-21 12:23:58 +0100

    moved pkg ver down

--- packages/util/src/constants/smartContract.ts
+++ packages/util/src/constants/smartContract.ts
@@ -2,24 +2,9 @@ export const xcAssetAbi = [
   {
     anonymous: false,
     inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
+      { indexed: true, internalType: "address", name: "owner", type: "address" },
+      { indexed: true, internalType: "address", name: "spender", type: "address" },
+      { indexed: false, internalType: "uint256", name: "value", type: "uint256" },
     ],
     name: "Approval",
     type: "event",
@@ -29,12 +14,7 @@ export const xcAssetAbi = [
     inputs: [
       { indexed: true, internalType: "address", name: "from", type: "address" },
       { indexed: true, internalType: "address", name: "to", type: "address" },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
+      { indexed: false, internalType: "uint256", name: "value", type: "uint256" },
     ],
     name: "Transfer",
     type: "event",
