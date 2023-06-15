c94987eaec16646190240eff3d178c2d0fd15559
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-06-15 10:22:11 +0100

    refactored Provider code

--- packages/cli/src/internal/providers.ts
+++ packages/cli/src/internal/providers.ts
@@ -20,131 +20,294 @@ import { ApiOptions } from "@polkadot/api/types/index.js";
 import { OverrideBundleType } from "@polkadot/types/types/registry";
 const debug = Debug("global:providers");
 
-//TODO: Make Generic /w function overloads
-export function prepareProviders(providerConfigs: ProviderConfig[]): MoonwallProvider[] {
-  return providerConfigs.map(({ name, endpoints, type, rpc, additionalTypes }) => {
-    const url = endpoints.includes("ENV_VAR") ? process.env.WSS_URL! : endpoints[0];
-    const privateKey = process.env.MOON_PRIV_KEY || ALITH_PRIVATE_KEY;
+class ProviderFactory {
+  private url: string;
+  private privateKey: string;
 
-    switch (type) {
+  constructor(private providerConfig: ProviderConfig) {
+    this.url = providerConfig.endpoints.includes("ENV_VAR")
+      ? process.env.WSS_URL!
+      : providerConfig.endpoints[0];
+    this.privateKey = process.env.MOON_PRIV_KEY || ALITH_PRIVATE_KEY;
+  }
+
+  public create(): MoonwallProvider {
+    switch (this.providerConfig.type) {
       case "polkadotJs":
-        debug(`🟢  PolkadotJs provider ${name} details prepared`);
-        return {
-          name,
-          type,
-          connect: async () => {
-            const options: ApiOptions = {
-              provider: new WsProvider(url),
-              initWasm: false,
-              noInitWarn: true,
-              rpc: !!rpc ? { ...rpcDefinitions, ...rpc } : rpcDefinitions,
-              typesBundle: !!additionalTypes ? additionalTypes : undefined,
-            };
-
-            if (!!rpc) {
-              options["rpc"] = rpc;
-            }
-
-            if (!!additionalTypes) {
-              options["typesBundle"] = { ...additionalTypes };
-            }
-
-            const api = await ApiPromise.create(options);
-            await api.isReady;
-            return api;
-          },
-          ws: () => new WsProvider(url),
-        };
-
+        return this.createPolkadotJs();
       case "moon":
-        debug(`🟢  Moonbeam provider ${name} details prepared`);
-        return {
-          name,
-          type,
-          connect: async () => {
-            const options: ApiOptions = {
-              provider: new WsProvider(url),
-              initWasm: false,
-              isPedantic: false,
-              rpc: !!rpc ? { ...rpcDefinitions, ...rpc } : rpcDefinitions,
-              typesBundle: types as OverrideBundleType,
-              noInitWarn: true,
-            };
-
-            const moonApi = await ApiPromise.create(options);
-            await moonApi.isReady;
-            return moonApi;
-          },
-          ws: () => new WsProvider(url),
-        };
-
+        return this.createMoon();
       case "web3":
-        debug(`🟢  Web3 provider ${name} details prepared`);
-        return {
-          name,
-          type,
-          connect: () => {
-            const provider = new Web3ProviderWs(
-              url,
-              {},
-              { delay: 50, autoReconnect: false, maxAttempts: 10 }
-            );
-
-            provider.on("error", () => {
-              throw new Error(
-                `Cannot connect to Web3 provider ${chalk.bgWhiteBright.blackBright(url)}`
-              );
-            });
-
-            return new Web3(provider);
-          },
-        };
-
+        return this.createWeb3();
       case "ethers":
-        debug(`🟢  Ethers provider ${name} details prepared`);
-        return {
-          name,
-          type,
-          connect: () => {
-            const provider = new ethers.WebSocketProvider(url);
-            return new Wallet(privateKey, provider);
-          },
-        };
-
+        return this.createEthers();
       case "viemPublic":
-        debug(`🟢  Viem Public provider ${name} details prepared`);
-        return {
-          name,
-          type,
-          connect: async () =>
-            createPublicClient({
-              transport: http(url.replace("ws", "http")),
-            }) as PublicViem,
-        };
-
+        return this.createViemPublic();
       case "viemWallet":
-        return {
-          name,
-          type,
-          connect: async () =>
-            createWalletClient({
-              chain: await deriveViemChain(url),
-              account: privateKeyToAccount(privateKey as `0x${string}`),
-              transport: http(url.replace("ws", "http")),
-            }) as WalletViem,
+        return this.createViemWallet();
+      default:
+        return this.createDefault();
+    }
+  }
+
+  private createPolkadotJs(): MoonwallProvider {
+    debug(`🟢  PolkadotJs provider ${this.providerConfig.name} details prepared`);
+    return {
+      name: this.providerConfig.name,
+      type: this.providerConfig.type,
+      connect: async () => {
+        const options: ApiOptions = {
+          provider: new WsProvider(this.url),
+          initWasm: false,
+          noInitWarn: true,
+          rpc: !!this.providerConfig.rpc ? this.providerConfig.rpc : undefined,
+          typesBundle: !!this.providerConfig.additionalTypes
+            ? this.providerConfig.additionalTypes
+            : undefined,
         };
 
-      default:
-        return {
-          name,
-          type,
-          connect: () => console.log(`🚧  provider ${name} not yet implemented`),
+        const api = await ApiPromise.create(options);
+        await api.isReady;
+        return api;
+      },
+      ws: () => new WsProvider(this.url),
+    };
+  }
+
+  private createMoon(): MoonwallProvider {
+    debug(`🟢  Moonbeam provider ${this.providerConfig.name} details prepared`);
+    return {
+      name: this.providerConfig.name,
+      type: this.providerConfig.type,
+      connect: async () => {
+        const options: ApiOptions = {
+          provider: new WsProvider(this.url),
+          initWasm: false,
+          isPedantic: false,
+          rpc: !!this.providerConfig.rpc
+            ? { ...rpcDefinitions, ...this.providerConfig.rpc }
+            : rpcDefinitions,
+          typesBundle: !!this.providerConfig.additionalTypes
+            ? { ...(types as OverrideBundleType), ...this.providerConfig.additionalTypes }
+            : (types as OverrideBundleType),
+          noInitWarn: true,
         };
-    }
-  });
+
+        const moonApi = await ApiPromise.create(options);
+        await moonApi.isReady;
+        return moonApi;
+      },
+      ws: () => new WsProvider(this.url),
+    };
+  }
+
+  private createWeb3(): MoonwallProvider {
+    debug(`🟢  Web3 provider ${this.providerConfig.name} details prepared`);
+    return {
+      name: this.providerConfig.name,
+      type: this.providerConfig.type,
+      connect: () => {
+        const provider = new Web3ProviderWs(
+          this.url,
+          {},
+          { delay: 50, autoReconnect: false, maxAttempts: 10 }
+        );
+
+        provider.on("error", () => {
+          throw new Error(
+            `Cannot connect to Web3 provider ${chalk.bgWhiteBright.blackBright(this.url)}`
+          );
+        });
+
+        return new Web3(provider);
+      },
+    };
+  }
+
+  private createEthers(): MoonwallProvider {
+    debug(`🟢  Ethers provider ${this.providerConfig.name} details prepared`);
+    return {
+      name: this.providerConfig.name,
+      type: this.providerConfig.type,
+      connect: () => {
+        const provider = new ethers.WebSocketProvider(this.url);
+        return new Wallet(this.privateKey, provider);
+      },
+    };
+  }
+
+  private createViemPublic(): MoonwallProvider {
+    debug(`🟢  Viem Public provider ${this.providerConfig.name} details prepared`);
+    return {
+      name: this.providerConfig.name,
+      type: this.providerConfig.type,
+      connect: async () =>
+        createPublicClient({
+          transport: http(this.url.replace("ws", "http")),
+        }) as PublicViem,
+    };
+  }
+
+  private createViemWallet(): MoonwallProvider {
+    debug(`🟢  Viem Wallet provider ${this.providerConfig.name} details prepared`);
+    return {
+      name: this.providerConfig.name,
+      type: this.providerConfig.type,
+      connect: async () =>
+        createWalletClient({
+          chain: await deriveViemChain(this.url),
+          account: privateKeyToAccount(this.privateKey as `0x${string}`),
+          transport: http(this.url.replace("ws", "http")),
+        }) as WalletViem,
+    };
+  }
+
+  private createDefault(): MoonwallProvider {
+    debug(`🟢  Default provider ${this.providerConfig.name} details prepared`);
+    return {
+      name: this.providerConfig.name,
+      type: this.providerConfig.type,
+      connect: () => console.log(`🚧  provider ${this.providerConfig.name} not yet implemented`),
+    };
+  }
 }
 
-//TODO: Make Generic /w function overloads
+export function prepareProviders(providerConfigs: ProviderConfig[]): MoonwallProvider[] {
+  return providerConfigs.map((providerConfig) => new ProviderFactory(providerConfig).create());
+}
+
+interface ProviderInterface {
+  name: string;
+  api: any;
+  type: ProviderType;
+  greet: () => void | Promise<void> | { rtVersion: number; rtName: string };
+  disconnect: () => void | Promise<void> | any;
+}
+
+class ProviderInterfaceFactory {
+  constructor(private name: string, private type: ProviderType, private connect: () => any) {}
+
+  public async create(): Promise<ProviderInterface> {
+    switch (this.type) {
+      case "polkadotJs":
+        return this.createPolkadotJs();
+      case "moon":
+        return this.createMoon();
+      case "web3":
+        return this.createWeb3();
+      case "ethers":
+        return this.createEthers();
+      case "viemPublic":
+        return this.createViemPublic();
+      case "viemWallet":
+        return this.createViemWallet();
+      default:
+        throw new Error("UNKNOWN TYPE");
+    }
+  }
+
+  private async createPolkadotJs(): Promise<ProviderInterface> {
+    const api = (await this.connect()) as ApiPromise;
+    return {
+      name: this.name,
+      api,
+      type: this.type,
+      greet: () => {
+        debug(
+          `👋  Provider ${this.name} is connected to chain` +
+            ` ${api.consts.system.version.specName.toString()} ` +
+            `RT${api.consts.system.version.specVersion.toNumber()}`
+        );
+        return {
+          rtVersion: api.consts.system.version.specVersion.toNumber(),
+          rtName: api.consts.system.version.specName.toString(),
+        };
+      },
+      disconnect: async () => api.disconnect(),
+    };
+  }
+
+  private async createMoon(): Promise<ProviderInterface> {
+    const api = (await this.connect()) as ApiPromise;
+    return {
+      name: this.name,
+      api,
+      type: this.type,
+      greet: () => {
+        debug(
+          `👋  Provider ${this.name} is connected to chain` +
+            ` ${api.consts.system.version.specName.toString()} ` +
+            `RT${api.consts.system.version.specVersion.toNumber()}`
+        );
+        return {
+          rtVersion: api.consts.system.version.specVersion.toNumber(),
+          rtName: api.consts.system.version.specName.toString(),
+        };
+      },
+      disconnect: async () => api.disconnect(),
+    };
+  }
+
+  private async createWeb3(): Promise<ProviderInterface> {
+    const api = (await this.connect()) as Web3;
+    return {
+      name: this.name,
+      api,
+      type: this.type,
+      greet: async () =>
+        console.log(
+          `👋 Provider ${this.name} is connected to chain ` + (await api.eth.getChainId())
+        ),
+      disconnect: async () => {
+        api.currentProvider!.disconnect();
+      },
+    };
+  }
+
+  private async createEthers(): Promise<ProviderInterface> {
+    const api = (await this.connect()) as Signer;
+    return {
+      name: this.name,
+      api,
+      type: this.type,
+      greet: async () =>
+        debug(
+          `👋  Provider ${this.name} is connected to chain ` +
+            (await api.provider!.getNetwork()).chainId
+        ),
+      disconnect: async () => api.provider!.destroy(),
+    };
+  }
+
+  private async createViemPublic(): Promise<ProviderInterface> {
+    const api = (await this.connect()) as PublicViem;
+    return {
+      name: this.name,
+      api,
+      type: this.type,
+      greet: async () =>
+        console.log(`👋 Provider ${this.name} is connected to chain ` + (await api.getChainId())),
+      disconnect: async () => {
+        // Not needed until we switch to websockets
+      },
+    };
+  }
+
+  private async createViemWallet(): Promise<ProviderInterface> {
+    const api = (await this.connect()) as WalletViem;
+    return {
+      name: this.name,
+      api,
+      type: this.type,
+      greet: async () =>
+        console.log(`👋 Provider ${this.name} is connected to chain ` + (await api.getChainId())),
+      disconnect: async () => {
+        // Not needed until we switch to websockets
+      },
+    };
+  }
+}
+
 export async function populateProviderInterface(
   name: string,
   type: ProviderType,
@@ -155,113 +318,6 @@ export async function populateProviderInterface(
     | Promise<PublicViem>
     | Promise<WalletViem>
     | void
-): Promise<{
-  name: string;
-  api: any;
-  type: ProviderType;
-  greet: () => void | Promise<void> | { rtVersion: number; rtName: string };
-  disconnect: () => void | Promise<void> | any;
-}> {
-  switch (type) {
-    case "polkadotJs":
-      const pjsApi = (await connect()) as ApiPromise;
-      return {
-        name,
-        api: pjsApi,
-        type,
-        greet: () => {
-          debug(
-            `👋  Provider ${name} is connected to chain` +
-              ` ${pjsApi.consts.system.version.specName.toString()} ` +
-              `RT${pjsApi.consts.system.version.specVersion.toNumber()}`
-          );
-          return {
-            rtVersion: pjsApi.consts.system.version.specVersion.toNumber(),
-            rtName: pjsApi.consts.system.version.specName.toString(),
-          };
-        },
-        disconnect: async () => pjsApi.disconnect(),
-      };
-
-    case "moon":
-      const mbApi = (await connect()) as ApiPromise;
-      return {
-        name,
-        api: mbApi,
-        type,
-        greet: () => {
-          debug(
-            `👋  Provider ${name} is connected to chain` +
-              ` ${mbApi.consts.system.version.specName.toString()} ` +
-              `RT${mbApi.consts.system.version.specVersion.toNumber()}`
-          );
-          return {
-            rtVersion: mbApi.consts.system.version.specVersion.toNumber(),
-            rtName: mbApi.consts.system.version.specName.toString(),
-          };
-        },
-        disconnect: async () => mbApi.disconnect(),
-      };
-
-    case "ethers":
-      const ethApi = (await connect()) as Signer;
-      return {
-        name,
-        api: ethApi,
-        type,
-        greet: async () =>
-          debug(
-            `👋  Provider ${name} is connected to chain ` +
-              (await ethApi.provider!.getNetwork()).chainId
-          ),
-        disconnect: async () => ethApi.provider!.destroy(),
-      };
-
-    case "web3":
-      const web3Api = (await connect()) as Web3;
-      return {
-        name,
-        api: web3Api,
-        type,
-        greet: async () =>
-          console.log(
-            `👋 Provider ${name} is connected to chain ` + (await web3Api.eth.getChainId())
-          ),
-        disconnect: async () => {
-          web3Api.currentProvider!.disconnect();
-        },
-      };
-    case "viemPublic":
-      const pubClient = (await connect()) as PublicViem;
-      return {
-        name,
-        api: pubClient,
-        type,
-        greet: async () =>
-          console.log(
-            `👋 Provider ${name} is connected to chain ` + (await pubClient.getChainId())
-          ),
-        disconnect: async () => {
-          // TODO: add disconnect
-        },
-      };
-
-    case "viemWallet":
-      const wallClient = (await connect()) as WalletViem;
-      return {
-        name,
-        api: wallClient,
-        type,
-        greet: async () =>
-          console.log(
-            `👋 Provider ${name} is connected to chain ` + (await wallClient.getChainId())
-          ),
-        disconnect: async () => {
-          //TODO: add disconnect
-        },
-      };
-
-    default:
-      throw new Error("UNKNOWN TYPE");
-  }
+): Promise<ProviderInterface> {
+  return await new ProviderInterfaceFactory(name, type, connect).create();
 }
