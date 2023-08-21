cc7cb9b5818a5c9abd72ed10ed91601366ccbb2c
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-08-21 11:25:24 +0100

    context upgrades

--- .changeset/brown-socks-trade.md
+++ .changeset/brown-socks-trade.md
@@ -3,4 +3,11 @@
 "@moonwall/cli": patch
 ---
 
-fix tanssi downloading
+Tanssi upgrades
+
+- Upgrading the artifact downloader to work for tanssi's non-semver release names
+- Added new context properties for `dev`` foundations
+  - `pjsApi`: Default getter for PolkadotJs ApiPromise
+  - `isEthereumChain` : Getter for whether chain is AccountId20 or not
+  - `isSubstrateChain` : Default getter for whether chain is AccountId32 or not
+  - `keyring`: Returns an object containing the default accounts (alice, bob)
---
+++ test/suites/tanssi/test_dev.ts
@@ -0,0 +1,40 @@
+import "@polkadot/api-augment";
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+
+describeSuite({
+  id: "D01",
+  title: "Dev test suite",
+  foundationMethods: "dev",
+  testCases: ({ it, context, log }) => {
+    beforeAll(async () => {});
+
+    it({
+      id: "T01",
+      title: "Checking that launched node can create blocks",
+      test: async function () {
+        const block = (
+          await context.polkadotJs().rpc.chain.getBlock()
+        ).block.header.number.toNumber();
+        await context.createBlock();
+        const block2 = (
+          await context.polkadotJs().rpc.chain.getBlock()
+        ).block.header.number.toNumber();
+        log(`Previous block #${block}, new block #${block2}`);
+        log("looking up account type");
+        log(`This is a substrate chain: ${context.isSubstrateChain}`);
+        log(`This is an ethereum chain: ${context.isEthereumChain}`);
+        expect(block2).to.be.greaterThan(block);
+      },
+    });
+
+    it({
+      id: "T02",
+      title: "Checking that default keyring is correct",
+      test: async function () {
+        expect(context.keyring.alice.address).toBe(
+          "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+        );
+      },
+    });
+  },
+});
