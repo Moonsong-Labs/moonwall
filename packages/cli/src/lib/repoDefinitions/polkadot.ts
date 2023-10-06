bb560867e6250da8daf56f770f50c4d6a7653860
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-10-06 13:09:59 +0100

    fixed zombienet CI

--- packages/cli/src/lib/repoDefinitions/polkadot.ts
+++ packages/cli/src/lib/repoDefinitions/polkadot.ts
@@ -2,7 +2,11 @@ import { RepoSpec } from "@moonwall/types";
 
 const repo: RepoSpec = {
   name: "polkadot",
-  binaries: [{ name: "polkadot" }],
+  binaries: [
+    { name: "polkadot" },
+    { name: "polkadot-prepare-worker" },
+    { name: "polkadot-execute-worker" },
+  ],
   ghAuthor: "paritytech",
   ghRepo: "polkadot-sdk",
 };
