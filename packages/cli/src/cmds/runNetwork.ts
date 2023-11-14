bdd4ef905afc05fdd8494537bc6307d50cc0665d
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-11-14 12:51:14 +0000

    fix for tail command

--- packages/cli/src/cmds/runNetwork.ts
+++ packages/cli/src/cmds/runNetwork.ts
@@ -439,7 +439,9 @@ const resolveTailChoice = async (env: Environment) => {
     switchNode = false;
     await new Promise(async (resolve) => {
       const onData = (chunk: any) => ui.log.write(chunk.toString());
-      const logFilePath = `${process.env.MOON_ZOMBIE_DIR}/${zombieNodes[zombieNodePointer]}.log`;
+      const logFilePath = process.env.MOON_ZOMBIE_NODES
+        ? `${process.env.MOON_ZOMBIE_DIR}/${zombieNodes[zombieNodePointer]}.log`
+        : process.env.MOON_LOG_LOCATION;
 
       // eslint-disable-next-line prefer-const
       let currentReadPosition = 0;
