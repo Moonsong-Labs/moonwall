d3ba4458a6dbfd29bf7f79da07a7bdd8dc914d7c
Tim B <79199034+timbrinded@users.noreply.github.com>
2023-11-20 11:03:16 +0000

    Restore Main to stable (#318)
    * fixed deps
    
    * chopsticks fix
    
    * removed effect
    
    * logging fix
    
    * vitest fix

--- packages/cli/src/cmds/entrypoint.ts
+++ packages/cli/src/cmds/entrypoint.ts
@@ -1,183 +1,130 @@
 import "../internal/logging";
 import "@moonbeam-network/api-augment";
 import yargs from "yargs";
-import fs from "fs";
 import { hideBin } from "yargs/helpers";
-import { testEffect } from "./runTests";
+import { testCmd } from "./runTests";
 import { runNetworkCmd } from "./runNetwork";
 import { generateConfig } from "../internal/cmdFunctions/initialisation";
+import { main } from "./main";
 import { fetchArtifact } from "../internal/cmdFunctions/fetchArtifact";
 import dotenv from "dotenv";
-import { Effect, Console, pipe } from "effect";
-import { main } from "./main";
 dotenv.config();
 
-const defaultConfigFiles = ["./moonwall.config", "./moonwall.config.json"];
-
-const findExistingConfig = (files: string[]): string | undefined => {
-  for (const file of files) {
-    if (fs.existsSync(file)) {
-      return file;
-    }
-  }
-};
-
-const defaultConfigFile = findExistingConfig(defaultConfigFiles) || "./moonwall.config.json";
-
-const parseConfigFile = Effect.sync(() =>
-  yargs(hideBin(process.argv))
-    .options({
-      configFile: {
-        type: "string",
-        alias: "c",
-        description: "path to MoonwallConfig file",
-        default: defaultConfigFile,
-      },
-    })
-    .parseSync()
-);
-
-const setEnvVar = (key: string, value: string) => Effect.sync(() => (process.env[key] = value));
-
-const setupConfigFileEnv = pipe(
-  parseConfigFile,
-  Effect.flatMap((parsed) => setEnvVar("MOON_CONFIG_PATH", parsed.configFile))
-);
-
-const cliStart = Effect.try(() => {
-  const argv = yargs(hideBin(process.argv))
-    .usage("Usage: $0")
-    .version("2.0.0")
-    .options({
-      configFile: {
-        type: "string",
-        alias: "c",
-        description: "path to MoonwallConfig file",
-        default: defaultConfigFile,
-      },
-    })
-    .parseSync();
-
-  if (!argv._.length) {
-    return main();
-  }
-
-  return yargs(hideBin(process.argv))
-    .usage("Usage: $0")
-    .version("2.0.0")
-    .options({
-      configFile: {
-        type: "string",
-        alias: "c",
-        description: "path to MoonwallConfig file",
-        default: defaultConfigFile,
-      },
-    })
-    .command(`init`, "Run tests for a given Environment", async () => {
-      const effect = Effect.tryPromise(() => generateConfig());
-
-      await Effect.runPromise(effect);
-    })
-    .command(
-      `download <bin> [ver] [path]`,
-      "Download x86 artifact from GitHub",
-      (yargs) => {
-        return yargs
-          .positional("bin", {
-            describe: "Name of artifact to download\n[ moonbeam | polkadot | *-runtime ]",
-          })
-          .positional("ver", {
-            describe: "Artifact version to download",
-            default: "latest",
-          })
-          .positional("path", {
-            describe: "Path where to save artifacts",
-            type: "string",
-            default: "./",
-          })
-          .option("overwrite", {
-            describe: "If file exists, should it be overwritten?",
-            type: "boolean",
-            alias: "d",
-            default: true,
-          })
-          .option("output-name", {
-            describe: "Rename downloaded file to this name",
-            alias: "o",
-            type: "string",
-          });
-      },
-      async (argv) => {
-        const effect = Effect.tryPromise(() => fetchArtifact(argv));
-        await Effect.runPromise(effect);
-      }
-    )
-    .command(
-      `test <envName> [GrepTest]`,
-      "Run tests for a given Environment",
-
-      (yargs) =>
-        yargs
-          .positional("envName", {
-            describe: "Network environment to run tests against",
-            array: true,
-            string: true,
-          })
-          .positional("GrepTest", {
-            type: "string",
-            description: "Pattern to grep test ID/Description to run",
-          }),
-
-      async ({ envName, GrepTest }) => {
-        process.env.MOON_RUN_SCRIPTS = "true";
-        const effect = Effect.gen(function* (_) {
-          if (envName) {
-            yield* _(testEffect(envName as any, { testNamePattern: GrepTest }));
-          } else {
-            console.error("👉 Run 'pnpm moonwall --help' for more information");
-            yield* _(Effect.fail("❌ No environment specified"));
-          }
-        });
-
-        await Effect.runPromise(effect);
-      }
-    )
-    .command(
-      `run <envName> [GrepTest]`,
-      "Start new network found in global config",
-      (yargs) =>
-        yargs
-          .positional("envName", {
-            describe: "Network environment to start",
-          })
-          .positional("GrepTest", {
-            type: "string",
-            description: "Pattern to grep test ID/Description to run",
-          }),
-      async (argv) => {
-        process.env.MOON_RUN_SCRIPTS = "true";
-        const effect = Effect.tryPromise(() => runNetworkCmd(argv as any));
-
-        await Effect.runPromise(effect);
-      }
-    )
-    .help("h")
-    .alias("h", "help")
-    .parse();
-});
-
-const cli = pipe(
-  setupConfigFileEnv,
-  Effect.flatMap(() => cliStart),
-  Effect.catchAll((error: any) => Effect.logError(`Error: ${error.message}`))
-);
-
-Effect.runPromise(cli)
-  .then((res) => {
-    Console.log(res);
-    process.exit(0);
+// Hack to expose config-path to all commands and fallback
+const parsed = yargs(hideBin(process.argv))
+  .options({
+    configFile: {
+      type: "string",
+      alias: "c",
+      description: "path to MoonwallConfig file",
+      default: "./moonwall.config.json",
+    },
   })
-  .catch((defect) => {
-    Console.error(defect);
-    process.exit(1);
-  });
+  .parseSync();
+process.env.MOON_CONFIG_PATH = parsed.configFile;
+
+yargs(hideBin(process.argv))
+  .usage("Usage: $0")
+  .version("2.0.0")
+  .options({
+    configFile: {
+      type: "string",
+      alias: "c",
+      description: "path to MoonwallConfig file",
+      default: "./moonwall.config.json",
+    },
+  })
+  .middleware((argv) => {
+    process.env.MOON_CONFIG_PATH = argv.configFile;
+  })
+  .command(`init`, "Run tests for a given Environment", async () => {
+    await generateConfig();
+  })
+  .command(
+    `download <bin> [ver] [path]`,
+    "Download x86 artifact from GitHub",
+    (yargs) => {
+      return yargs
+        .positional("bin", {
+          describe: "Name of artifact to download\n[ moonbeam | polkadot | *-runtime ]",
+        })
+        .positional("ver", {
+          describe: "Artifact version to download",
+          default: "latest",
+        })
+        .positional("path", {
+          describe: "Path where to save artifacts",
+          type: "string",
+          default: "./",
+        })
+        .option("overwrite", {
+          describe: "If file exists, should it be overwritten?",
+          type: "boolean",
+          alias: "d",
+          default: true,
+        })
+        .option("output-name", {
+          describe: "Rename downloaded file to this name",
+          alias: "o",
+          type: "string",
+        });
+    },
+    async (argv) => {
+      await fetchArtifact(argv as any);
+    }
+  )
+  .command(
+    `test <envName> [GrepTest]`,
+    "Run tests for a given Environment",
+    (yargs) => {
+      return yargs
+        .positional("envName", {
+          describe: "Network environment to run tests against",
+          array: true,
+          string: true,
+        })
+        .positional("GrepTest", {
+          type: "string",
+          description: "Pattern to grep test ID/Description to run",
+        });
+    },
+    async (args) => {
+      if (args.envName) {
+        process.env.MOON_RUN_SCRIPTS = "true";
+        (await testCmd(args.envName.toString(), { testNamePattern: args.GrepTest }))
+          ? process.exit(0)
+          : process.exit(1);
+      } else {
+        console.log("❌ No environment specified");
+        console.log(`👉 Run 'pnpm moonwall --help' for more information`);
+        process.exit(1);
+      }
+    }
+  )
+  .command(
+    `run <envName> [GrepTest]`,
+    "Start new network found in global config",
+    (yargs) => {
+      return yargs
+        .positional("envName", {
+          describe: "Network environment to start",
+        })
+        .positional("GrepTest", {
+          type: "string",
+          description: "Pattern to grep test ID/Description to run",
+        });
+    },
+    async (argv) => {
+      process.env.MOON_RUN_SCRIPTS = "true";
+      await runNetworkCmd(argv as any);
+      process.exit(0);
+    }
+  )
+  .demandCommand(1)
+  .fail(async (msg) => {
+    console.log(msg);
+    await main();
+  })
+  .help("h")
+  .alias("h", "help")
+  .parse();
