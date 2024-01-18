bdd3777f252143deb7f91f4e1f04bfb42b21b9c1
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-01-18 10:42:23 +0000

    lint

--- packages/util/src/functions/jumping.ts
+++ packages/util/src/functions/jumping.ts
@@ -1,25 +1,5 @@
 import "@moonbeam-network/api-augment";
-import { ChopsticksContext, DevModeContext } from "@moonwall/types";
-
-export async function jumpToRoundDev(
-  context: DevModeContext,
-  round: number
-): Promise<string | null> {
-  let lastBlockHash = "";
-  for (;;) {
-    const currentRound = (
-      await context.polkadotJs().query.parachainStaking.round()
-    ).current.toNumber();
-
-    if (currentRound === round) {
-      return lastBlockHash;
-    } else if (currentRound > round) {
-      return null;
-    }
-
-    lastBlockHash = (await context.createBlock()).block.hash.toString();
-  }
-}
+import { DevModeContext } from "@moonwall/types";
 
 export async function jumpBlocksDev(context: DevModeContext, blockCount: number) {
   while (blockCount > 0) {
@@ -37,4 +17,21 @@ export async function jumpRoundsDev(
     .toNumber();
 
   return jumpToRoundDev(context, round);
+}
+
+async function jumpToRoundDev(context: DevModeContext, round: number): Promise<string | null> {
+  let lastBlockHash = "";
+  for (;;) {
+    const currentRound = (
+      await context.polkadotJs().query.parachainStaking.round()
+    ).current.toNumber();
+
+    if (currentRound === round) {
+      return lastBlockHash;
+    } else if (currentRound > round) {
+      return null;
+    }
+
+    lastBlockHash = (await context.createBlock()).block.hash.toString();
+  }
 }
