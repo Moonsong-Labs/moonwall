ad8028f3fc8b5ffa47aa3b2d785e229d490aaf66
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-11-27 14:30:43 +0000

    test: :white_check_mark: add fork test

--- test/suites/fork_test/test_fork.ts
+++ test/suites/fork_test/test_fork.ts
@@ -8,17 +8,15 @@ describeSuite({
   foundationMethods: "dev",
   options: {
     forkConfig: {
-      url: "https://moonbeam.public.blastapi.io",
-      verbose: true
+      url: "https://moonbeam.unitedbloc.com",
+      verbose: true,
     },
   },
-  testCases: ({ it, context , log}) => {
+  testCases: ({ it, context, log }) => {
     let polkadotJs: ApiPromise;
 
     beforeAll(async () => {
       polkadotJs = context.polkadotJs();
-      log("Waiting a minute for lazy loading to do its thing");
-      await new Promise((resolve) => setTimeout(resolve, 60_000)); // wait for  LL loading 1 minute
     });
 
     it({
@@ -26,9 +24,23 @@ describeSuite({
       title: "Checking that launched node can create blocks",
       test: async () => {
         const block = (await polkadotJs.rpc.chain.getBlock()).block.header.number.toNumber();
-        await context.createBlock();
+        await context.createBlock([], { finalize: false });
         const block2 = (await polkadotJs.rpc.chain.getBlock()).block.header.number.toNumber();
         expect(block2).to.be.greaterThan(block);
+      },
+    });
+
+    it({
+      id: "T02",
+      title: "Check that state overrides work",
+      test: async () => {
+        const testAccount = "0x8300db2442725604b8f5Eb172692Bb15078205c2";
+        log(`Address: ${testAccount}`);
+
+        const {
+          data: { free },
+        } = await polkadotJs.query.system.account(testAccount);
+        expect(free.toBigInt(), "Free balance should be 1337000").toBe(1337000000000000000000n);
       },
     });
   },
